bdbc986a911b2495836cded3b7f5f6f3e2a78c3c
yliff <yaakovliff@gmail.com>
2017-11-07 18:39:10 -0500

    commit a

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -1,16 +1,23 @@
 import org.junit.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
     @org.junit.Before
     public void setUp() throws Exception {
-
+        LinkedList<Object> list = new LinkedList();
     }
 
     @Test
     public void aVoid(){
         assertTrue(false);
+    }
+
+    @Test
+    public void addToListTest(){
+
     }
 
 }

847199ae5a3712d8490641802abe8d15b201f4d5
dovi bootable <dcoh18@gmail.com>
2017-11-07 23:37:16 +0000

    .

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -1,8 +1,16 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
     @org.junit.Before
     public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void aVoid(){
+        assertTrue(false);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        LinkedList<String> list = new LinkedList<>();
-
-    }
-
-    public static void testList(LinkedList list){
-        list.add("");
     }
 }

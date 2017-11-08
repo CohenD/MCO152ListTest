0a84d202432d7f73c521c16569c46f0809ccc1eb
yliff <yaakovliff@gmail.com>
2017-11-07 19:43:57 -0500

    commit add-addAll-clear

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -12,15 +12,46 @@ public class MainTest {
     public void setUp() throws Exception {
          list = new LinkedList<>();
          for (int i = 0; i < 10; i++){
-             list.add(" " + i);
+             list.add("" + i);
          }
     }
 
 
     @Test
-    public void addToEndOfListTest(){
-        list.add("a");
-        assertTrue(list.size() == 1);
+    public void linkedList_addToEndOfListTest(){
+
+        list.add("1");
+        assertTrue(list.get(10).equals("1"));
     }
 
+    @Test                   //addAll
+    public void linkedList_addColletionToEndOfList_elementInPostionTest(){
+        LinkedList<String> list2 = new LinkedList<>();
+        list2.add("100");
+        list2.add("200");
+
+        list.addAll(list2);
+        System.out.println(list.size());
+        assertTrue(list.get(10).equals("100"));
+    }
+
+    @Test                   //addAll
+    public void linkedList_addColletionToEndOfList_noElementsDeletedTest(){
+        LinkedList<String> list2 = new LinkedList<>();
+        list2.add("100");
+        list2.add("200");
+
+        list.addAll(list2);
+        System.out.println(list.size());
+        assertTrue(list.size() == 12);
+    }
+
+
+
+    @Test
+    public void clearTest(){
+
+        list.clear();
+        assertTrue(list.size() == 0);
+    }
 }

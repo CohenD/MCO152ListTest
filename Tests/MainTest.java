40db87108a24c0cfcbbe61ea7236a5e566c41d18
dovi bootable <dcoh18@gmail.com>
2017-11-08 00:41:50 +0000

    contains

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -12,15 +12,35 @@ public class MainTest {
     public void setUp() throws Exception {
          list = new LinkedList<>();
          for (int i = 0; i < 10; i++){
-             list.add(" " + i);
+             list.add("" + i);
          }
     }
 
+    @Test
+    public void testElementSpecifiedPosition(){
+        list.add(3 ,"a");
+        assertTrue(list.get(3).equals("a"));
+    }
 
     @Test
-    public void addToEndOfListTest(){
-        list.add("a");
-        assertTrue(list.size() == 1);
+    public void testCollectionSpecifiedPosition(){
+        String[] collection = {"abc", "def"};
+
+//        list.add(3 , collection);
+        assertTrue(list.get(3).equals("a"));
     }
+
+    //////////////////////////////////////////////
+    @Test
+    public void testContainsElement(){
+        assertTrue(list.contains("1"));
+    }
+    @Test
+    public void testDoesntContaintElement(){
+        assertFalse(list.contains("12"));
+    }
+    //////////////////////////////////////////////
+
+
 
 }

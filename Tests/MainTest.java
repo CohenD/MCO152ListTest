25b8fd3661fb95cc13e354124d5e4c0df17bbcd0
dovi bootable <dcoh18@gmail.com>
2017-11-08 01:04:23 +0000

    .

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -40,6 +40,59 @@ public class MainTest {
         assertFalse(list.contains("12"));
     }
     //////////////////////////////////////////////
+    @Test
+    public void listEquals(){
+        //Note this is deep because contents are String which are immutable
+        LinkedList<String> list2 = (LinkedList<String>) list.clone();
+
+        assertTrue(list2.equals(list));
+    }
+
+    @Test
+    public void listNotEquals(){
+        //Note this is deep because contents are String which are immutable
+        LinkedList<String> list2 = (LinkedList<String>) list.clone();
+
+        list2.set(0, "a");
+
+        assertFalse(list2.equals(list));
+    }
+    /////////////////////////////////////////////
+    //test hashCode()????
+    ////////////////////////////////////////////
+
+    @Test
+    public void testIsEmpty(){
+        list = new LinkedList<>();
+        assertTrue(list.isEmpty());
+    }
+
+    /////////////////////////////////////////////
+    @Test
+    public void testLastIndexAtEnd(){
+        list.set(list.size() -1, "abcd");
+        assertTrue(list.lastIndexOf("abcd") == list.size() - 1);
+    }
+
+    @Test
+    public void testLastIndexAtMiddle(){
+        list.set(5, "abcd");
+        assertTrue(list.lastIndexOf("abcd") == 5);
+    }
+
+    @Test
+    public void testLastIndexAtBegining(){
+        list.set(0, "abcd");
+        assertTrue(list.lastIndexOf("abcd") == 0);
+    }
+    ///////////////////////////////////////////////
+    //Listiterator()
+    //////////////////////////////////////////////
+
+
+
+
+
 
 
 
